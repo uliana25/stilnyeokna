a1384f6f705372cbe3240e6612ca46cdeaa3c193
Deniz True <nzxt.ua@gmail.com>
2017-11-23 20:29:23 +0200

    Header logo and Navigation menu.

--- pages/services.vue
+++ pages/services.vue
@@ -1,7 +1,8 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <img src="/window.png" alt="StilnyeOkna.com" height="250px" class="mb-5" />
+
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
