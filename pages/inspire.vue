174293e68975a3eca1f241d596a4df62b2f15f8b
Uliana25 <ulianazhernokleva@gmail.com>
2017-11-24 01:54:40 +0200

    Carousel

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,52 +1,29 @@
 <template>
   <v-container fluid>
     <v-layout row>
-      <v-flex xs4>
-        <v-card>
-          <v-card-media
-          class="grey--text"
-            height="270px"
-            src="window.png"
-            
-          >
-          </v-card-media>
-          <v-card-text>
-            <div>
-                <router-link :to="{ name: 'index' }"> <h4 class="text-xs-center">МПО</h4></router-link>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs4>
-          <v-card>
-            <v-card-media
-            class="grey--text"
-              height="270px"
-              src="maf.jpg"
-            >
-            </v-card-media>
-            <v-card-text>
-              <div>
-                  <router-link :to="{ name: 'index' }"> <h4 class="text-xs-center">МАФЫ</h4></router-link>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex xs4>
-            <v-card>
-              <v-card-media
-              class="grey--text"
-                height="270px"
-                src="rollet.jpg"
-              >
-              </v-card-media>
-              <v-card-text>
-                <div>
-                    <router-link :to="{ name: 'index' }"> <h4 class="text-xs-center">Роллеты</h4></router-link>
-                </div>
-              </v-card-text>
-            </v-card>
-          </v-flex>
+      <v-carousel>
+        <v-carousel-item v-for="(item,i) in items" v-bind:src="item.src" :key="i"></v-carousel-item>
+      </v-carousel>
     </v-layout>
-  </v-container>
-</template>  
+  </v-container>  
+</template>
+
+export default {
+  data () {
+    return {
+      items: [
+        {
+          src: '/slide1.jpg'
+        },
+        {
+          src: '/slide2.jpg'
+        },
+        {
+          src: '/slide3.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+  
